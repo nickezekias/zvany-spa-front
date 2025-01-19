efc0bbfb5b6640c2bc592e064b48fda78ccb321a
Nick Ezekiel <nickezekias@gmail.com>
2025-01-19 03:48:11 +0100

    feat:added guest layout and restructure layout for improved UI

---
+++ src/layouts/guest/IndexLayout.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import PrimeToolbar from 'primevue/toolbar'
+</script>
+
+<template>
+  <div class="h-screen w-screen flex-col">
+    <PrimeToolbar class="py-0 rounded-none border-none border-b">
+      <template #start>
+        <div class="logo">LugShare</div>
+      </template>
+
+      <template #end>
+        <!-- <router-link to="/register" activeClass="border-b border-primary" class="py-2">
+          <PrimeButton text plain class="" :label="$t('labels.register')" />
+        </router-link> -->
+
+        <router-link to="/login" activeClass="border-b border-primary" class="ml-3 py-2">
+          <PrimeButton text plain class="" :label="$t('labels.login')" />
+        </router-link>
+      </template>
+    </PrimeToolbar>
+
+    <main class="mt-4 md:mt-16">
+      <RouterView />
+    </main>
+  </div>
+</template>
