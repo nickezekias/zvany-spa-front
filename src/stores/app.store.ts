ac57cbb8268eda4f81b25610bf2197f4cd629c67
Nick Ezekiel <nickezekias@gmail.com>
2025-01-28 22:49:00 +0100

    refactor: removed toast functions that have nothing to do with state

--- src/stores/app.store.ts
+++ src/stores/app.store.ts
@@ -1,53 +1,16 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import { useI18n } from 'vue-i18n'
-import { useToast } from 'primevue'
-
-import { AxiosError as HttpError } from 'axios'
-import { getApiErrors } from '@/app/utils/helpers'
 
 export const useAppStore = defineStore('appStore', () => {
   const appLoading = ref(false)
-  const toast = useToast()
-  const { t } = useI18n()
 
   const setAppLoading = (value: boolean) => {
     appLoading.value = value
-  }
-
-  const toastAPIError = (error: HttpError, title = 'labels.operationFailure') => {
-    toast.add({
-      severity: 'error',
-      summary: t(`${title}`),
-      detail: t(`${getApiErrors(error)}`),
-      life: 10000,
-    })
-  }
-
-  const toastError = (detail = '', title = 'labels.operationFailure') => {
-    toast.add({
-      severity: 'error',
-      summary: t(`${title}`),
-      detail: t(`${detail}`),
-      life: 10000,
-    })
-  }
-
-  const toastSuccess = (detail = '', title = 'labels.operationSuccess') => {
-    toast.add({
-      severity: 'success',
-      summary: t(`${title}`),
-      detail: t(`${detail}`),
-      life: 5000,
-    })
   }
 
   return {
     appLoading,
 
     setAppLoading,
-    toastAPIError,
-    toastError,
-    toastSuccess,
   }
 })
