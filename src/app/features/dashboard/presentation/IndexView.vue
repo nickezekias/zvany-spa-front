ce14225b99a75b73ad90669dc56c36231a77466e
Nick Ezekiel <nickezekias@gmail.com>
2025-01-28 22:48:31 +0100

    added basic empty home and dashboard views

---
+++ src/app/features/dashboard/presentation/IndexView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Welcome to your Dashboard</h1>
+</template>
---
+++ src/app/features/home/presentation/IndexView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Welcome Home</h1>
+</template>
