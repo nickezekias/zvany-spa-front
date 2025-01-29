bd70c5a47f2feabf8f7b3e3ae56d29032ba55bf6
Nick Ezekiel <nickezekias@gmail.com>
2025-01-29 13:29:12 +0100

    feat: added chat and notification drawers with localization support

---
+++ src/components/chat/ChatDrawer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Ref } from 'vue'
+
+import PrimeDrawer from 'primevue/drawer'
+
+const model: Ref<boolean> = defineModel() as Ref<boolean>
+</script>
+
+<template>
+  <PrimeDrawer v-model:visible="model" position="right">
+    <template #header>
+      <h2>{{ $t('labels.message', 2) }}</h2>
+    </template>
+  </PrimeDrawer>
+</template>
