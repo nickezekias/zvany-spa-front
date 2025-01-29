88bc4417f98c2007ed1f1def9a2015773f73ca31
Nick Ezekiel <nickezekias@gmail.com>
2025-01-29 20:18:30 +0100

    feat: enhance KYCComponent with improved layout and localization for ID document upload

--- src/components/user/KYCComponent.vue
+++ src/components/user/KYCComponent.vue
@@ -3,7 +3,10 @@ import FileUploader from '../FileUploader.vue'
 </script>
 
 <template>
-  <div>
-    <FileUploader />
+  <div class="nikk-container">
+    <h1 class="text-2xl font-semibold">{{ $t('labels.uploadIDDocument') }}</h1>
+    <div class="flex w-full md:w-1/3 pt-4">
+      <FileUploader />
+    </div>
   </div>
 </template>
