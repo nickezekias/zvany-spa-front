e6fd62710373ef12e55648df29e0e2f166a013e6
Nick Ezekiel <nickezekias@gmail.com>
2025-02-21 09:01:30 +0100

    feat: add SearchBarComponent and enhance IndexView with hero section and localization

--- src/app/features/listing/presentation/IndexView.vue
+++ src/app/features/listing/presentation/IndexView.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchBarComponent from '@/components/listings/SearchBarComponent.vue'
+</script>
 
 <template>
-  <div>Listings Index</div>
+  <div>
+    <div
+      class="hero min-h-96 bg-primary flex flex-col p-6 md:px-32 lg:px-64 gap-4 items-center justify-center"
+    >
+      <div class="text-center">
+        <h1 class="text-xl md:text-4xl font-bold text-white">
+          {{ $t('features.listings.index.hero.title') }}
+        </h1>
+        <p class="text-sm md:text-lg text-white">
+          {{ $t('features.listings.index.hero.subtitle') }}
+        </p>
+      </div>
+      <SearchBarComponent />
+    </div>
+  </div>
 </template>
