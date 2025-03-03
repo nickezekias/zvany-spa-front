0c28eb56bce918756d7ddaefa3c59d27c9fa62f9
Nick Ezekiel <nickezekias@gmail.com>
2025-03-03 06:42:07 +0100

    feat: refactor router added scrollToTop and configuration to use RouteRecordRaw for improved type safety

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import middlewarePipeline from './middleware/middlewarePipeline'
 import guest from './middleware/guest.middleware'
 
@@ -7,90 +7,101 @@ import { useAppStore } from '@/stores/app.store'
 import GuestLayout from '@/layouts/guest/IndexLayout.vue'
 import auth from './middleware/auth.middleware'
 
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: GuestLayout,
+    meta: { middleware: [guest] },
+    children: [
+      {
+        path: '',
+        redirect: '/login',
+      },
+      {
+        path: '/forgot-password',
+        name: 'account.forgotPassword',
+        component: () =>
+          import('@/app/features/account/forgot-password/presentation/IndexView.vue'),
+      },
+      {
+        path: '/login',
+        name: 'account.login',
+        component: () => import('@/app/features/account/login/presentation/IndexView.vue'),
+      },
+      {
+        path: '/register',
+        name: 'account.register',
+        component: () => import('@/app/features/account/register/presentation/IndexView.vue'),
+      },
+      {
+        path: '/reset-password',
+        name: 'account.resetPassword',
+        component: () => import('@/app/features/account/reset-password/presentation/IndexView.vue'),
+      },
+    ],
+  },
+  {
+    path: '/onboarding',
+    name: 'onboarding.index',
+    component: () => import('@/app/features/onboarding/presentation/IndexView.vue'),
+  },
+  {
+    path: '/',
+    component: () => import('@/layouts/app/IndexLayout.vue'),
+    meta: { middleware: [auth] },
+    children: [
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/app/features/dashboard/presentation/IndexView.vue'),
+      },
+      {
+        path: '/u',
+        children: [
+          {
+            path: 'public-profile',
+            name: 'user.publicProfile',
+            component: () => import('@/app/features/profile/public/presentation/IndexView.vue'),
+          },
+          {
+            path: 'profile',
+            name: 'user.profile',
+            component: () => import('@/app/features/profile/presentation/IndexView.vue'),
+          },
+        ],
+      },
+      {
+        path: '/listings',
+        children: [
+          {
+            path: '',
+            name: 'listings.index',
+            component: () => import('@/app/features/listing/presentation/IndexView.vue'),
+          },
+          {
+            path: 'create',
+            name: 'listings.create',
+            component: () => import('@/app/features/listing/presentation/CreateView.vue'),
+          },
+          {
+            path: 'edit/:id',
+            name: 'listings.edit',
+            component: () => import('@/app/features/listing/presentation/EditView.vue'),
+          },
+        ],
+      },
+    ],
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: GuestLayout,
-      meta: { middleware: [guest] },
-      children: [
-        {
-          path: '',
-          redirect: '/login',
-        },
-        {
-          path: '/forgot-password',
-          name: 'account.forgotPassword',
-          component: () =>
-            import('@/app/features/account/forgot-password/presentation/IndexView.vue'),
-        },
-        {
-          path: '/login',
-          name: 'account.login',
-          component: () => import('@/app/features/account/login/presentation/IndexView.vue'),
-        },
-        {
-          path: '/register',
-          name: 'account.register',
-          component: () => import('@/app/features/account/register/presentation/IndexView.vue'),
-        },
-        {
-          path: '/reset-password',
-          name: 'account.resetPassword',
-          component: () =>
-            import('@/app/features/account/reset-password/presentation/IndexView.vue'),
-        },
-      ],
-    },
-    {
-      path: '/onboarding',
-      name: 'onboarding.index',
-      component: () => import('@/app/features/onboarding/presentation/IndexView.vue'),
-    },
-    {
-      path: '/',
-      component: () => import('@/layouts/app/IndexLayout.vue'),
-      meta: { middleware: [auth] },
-      children: [
-        {
-          path: '/dashboard',
-          name: 'dashboard',
-          component: () => import('@/app/features/dashboard/presentation/IndexView.vue'),
-        },
-        {
-          path: '/u',
-          children: [
-            {
-              path: 'public-profile',
-              name: 'user.publicProfile',
-              component: () => import('@/app/features/profile/public/presentation/IndexView.vue'),
-            },
-            {
-              path: 'profile',
-              name: 'user.profile',
-              component: () => import('@/app/features/profile/presentation/IndexView.vue'),
-            },
-          ],
-        },
-        {
-          path: '/listings',
-          children: [
-            {
-              path: '',
-              name: 'listings.index',
-              component: () => import('@/app/features/listing/presentation/IndexView.vue'),
-            },
-            {
-              path: 'create',
-              name: 'listings.create',
-              component: () => import('@/app/features/listing/presentation/CreateView.vue'),
-            },
-          ],
-        },
-      ],
-    },
-  ],
+  routes,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  scrollBehavior(to, from, savedPosition) {
+    // Always scroll to the top on navigation
+    return { top: 0 }
+  },
 })
 
 router.beforeEach(async (to, from, next) => {
