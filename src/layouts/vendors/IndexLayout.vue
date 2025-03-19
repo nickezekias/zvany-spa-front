5983b6e7c09a9e4af69163bd74f5b1b4310b067e
Nick Ezekiel <nickezekias@gmail.com>
2025-03-19 18:32:17 +0100

    feat: added vendor layout

---
+++ src/layouts/vendors/IndexLayout.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import PrimeToast from 'primevue/toast'
+import TopBar from '@/layouts/vendors/TopBar.vue'
+</script>
+
+<template>
+  <div class="flex">
+    <PrimeToast />
+    <div id="app" class="w-full">
+      <div class="flex bg-surface-50 dark:bg-[#121212] w-screen overflow-hidden">
+        <!-- Content -->
+        <div class="main-content">
+          <div class="flex flex-col h-screen">
+            <!-- Top Bar -->
+            <TopBar />
+
+            <div class="flex-1 overflow-y-auto">
+              <RouterView />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-content {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+}
+
+@media (max-width: 768px) {
+  .main-content {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/layouts/vendors/TopBar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useAppStore } from '@/stores/app.store'
+import UserAvatarMenu from '@/components/user/UserAvatarMenu.vue'
+
+const appStore = useAppStore()
+</script>
+<template>
+  <PrimeToolbar class="py-0 px-[3rem] rounded-none">
+    <template #start>
+      <div class="logo">
+        <router-link to="/">
+          <span class="font-bold text-xl">{{ appStore.appName }}</span>
+        </router-link>
+      </div>
+    </template>
+
+    <template #center>
+      <router-link
+        :to="{ name: 'vendors.products.index' }"
+        exactActiveClass="border-b border-primary"
+        class="hidden md:flex py-2"
+      >
+        <PrimeButton text plain :label="$t('labels.product', 2)" />
+      </router-link>
+    </template>
+
+    <template #end>
+      <div class="flex align-items-center gap-2">
+        <UserAvatarMenu />
+      </div>
+    </template>
+  </PrimeToolbar>
+</template>
