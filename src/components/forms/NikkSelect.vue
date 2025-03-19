40d58d2044176dd0033a830723cd703ebc1fdad7
Nick Ezekiel <nickezekias@gmail.com>
2025-03-19 20:47:14 +0100

    [WIP] added float label for NikkSelect component

--- src/components/forms/NikkSelect.vue
+++ src/components/forms/NikkSelect.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import PrimeFloatLabel from 'primevue/floatlabel'
 import PrimeSelect from 'primevue/select'
 
 const model = defineModel()
@@ -8,6 +9,8 @@ const props = defineProps<{
   hint?: string
   id: string
   isError?: boolean
+  label: string
+  literalLabel?: string
   loading?: boolean
   name?: string
   optionLabel?: string
@@ -20,42 +23,46 @@ const props = defineProps<{
 
 <template>
   <div>
-    <PrimeSelect
-      v-model="model"
-      class="w-full"
-      :disabled="props.disabled"
-      :hint="props.hint"
-      :invalid="props.isError"
-      :loading="props.loading"
-      :name="props.name"
-      :optionLabel="props.optionLabel"
-      :optionValue="props.optionValue"
-      :options="props.options"
-      :placeholder="props.placeholder ? $t(`${props.placeholder}`) : ''"
-      :readonly="props.readonly"
-    >
-      <template #value="slotProps">
-        <slot
-          v-if="
-            (Array.isArray(slotProps.value) && slotProps.value.length > 0) ||
-            (!Array.isArray(slotProps.value) && slotProps.value)
-          "
-          name="value"
-          :slotProps="slotProps"
-        >
-          {{ slotProps.value }}
-        </slot>
-        <span v-else>
-          {{ slotProps.placeholder }}
-        </span>
-      </template>
+    <PrimeFloatLabel variant="on">
+      <PrimeSelect
+        v-model="model"
+        class="w-full"
+        :disabled="props.disabled"
+        :hint="props.hint"
+        :invalid="props.isError"
+        :loading="props.loading"
+        :name="props.name"
+        :optionLabel="props.optionLabel"
+        :optionValue="props.optionValue"
+        :options="props.options"
+        :placeholder="props.placeholder ? $t(`${props.placeholder}`) : ''"
+        :readonly="props.readonly"
+      >
+        <template #value="slotProps">
+          <slot
+            v-if="
+              (Array.isArray(slotProps.value) && slotProps.value.length > 0) ||
+              (!Array.isArray(slotProps.value) && slotProps.value)
+            "
+            name="value"
+            :slotProps="slotProps"
+          >
+            {{ slotProps.value }}
+          </slot>
+          <span v-else>
+            {{ slotProps.placeholder }}
+          </span>
+        </template>
 
-      <template #option="slotProps">
-        <slot name="option" :slotProps="slotProps">
-          {{ slotProps.option.label ? slotProps.option.label : slotProps.option }}
-        </slot>
-      </template>
-    </PrimeSelect>
+        <template #option="slotProps">
+          <slot name="option" :slotProps="slotProps">
+            {{ slotProps.option.label ? slotProps.option.label : slotProps.option }}
+          </slot>
+        </template>
+      </PrimeSelect>
+      <!-- <label v-if="!props.literalLabel" :for="props.id">{{ $t(`${props.label}`) }}</label> -->
+      <!-- <label v-else :for="props.id">{{ props.literalLabel }}</label> -->
+    </PrimeFloatLabel>
     <PrimeMessage v-if="props.isError" severity="error" size="small" variant="simple">
       {{ $t(`${props.errorHelpLabel}`) }}
     </PrimeMessage>
