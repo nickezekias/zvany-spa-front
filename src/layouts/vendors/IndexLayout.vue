41ba8dedd238b4ae8ea3694d7b8afa872c070936
Nick Ezekiel <nickezekias@gmail.com>
2025-03-20 11:50:13 +0100

    refactor: simplify IndexLayout structure and remove unused styles

--- src/layouts/vendors/IndexLayout.vue
+++ src/layouts/vendors/IndexLayout.vue
@@ -4,38 +4,31 @@ import TopBar from '@/layouts/vendors/TopBar.vue'
 </script>
 
 <template>
-  <div class="flex">
+  <div class="min-h-screen flex flex-col bg-surface-50 dark:bg-[#121212]">
     <PrimeToast />
-    <div id="app" class="w-full">
-      <div class="flex bg-surface-50 dark:bg-[#121212] w-screen overflow-hidden">
-        <!-- Content -->
-        <div class="main-content">
-          <div class="flex flex-col h-screen">
-            <!-- Top Bar -->
-            <TopBar />
+    <TopBar />
 
-            <div class="flex-1 overflow-y-auto">
+    <div class="flex flex-grow">
+      <main class="flex-grow max-w-screen pb-[100vh] md:pb-32 overflow-x-hidden">
+        <router-view />
+      </main>
+    </div>
+
+    <!-- <div id="app" class="w-full">
+      <div class="flex bg-surface-50 dark:bg-[#121212] w-screen overflow-y-auto">
+        Content
+        <div class="main-content">
+          <div class="h-full">
+           Top Bar
+
+            <div class="flex-1">
               <RouterView />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
-<style scoped>
-.main-content {
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  height: 100vh;
-  overflow-x: hidden;
-}
-
-@media (max-width: 768px) {
-  .main-content {
-    width: 100%;
-  }
-}
-</style>
+<style scoped></style>
