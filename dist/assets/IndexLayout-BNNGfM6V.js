5ecd8c84f70d2c257bea445ee90edd033ce9c4a7
Nick Ezekiel <nickezekias@gmail.com>
2025-03-22 16:15:33 +0100

    ran build

---
+++ dist/assets/IndexLayout-BNNGfM6V.js
@@ -0,0 +1,1 @@
+import{_ as f}from"./index-BAwMGmj-.js";export{f as default};
--- dist/assets/UserAvatar.vue_vue_type_script_setup_true_lang-CgcHn-Xt.js
+++
@@ -1,1 +0,0 @@
-import{d as o,u as t,g as s,o as n,D as c,i as l}from"./index-CSluO9mJ.js";const u=o({__name:"UserAvatar",setup(i){const r=t();return(p,m)=>{var e;const a=s("PrimeAvatar");return n(),c(a,{label:(e=l(r).userModel)==null?void 0:e.initials,shape:"circle",class:"text-primary bg-[#0099ff10] border-2 border-primary"},null,8,["label"])}}});export{u as _};
